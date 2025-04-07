ab726693930f848d9e5135bb28f52b15446a083e
HasegawaShuhei <sh1027.dev@gmail.com>
2025-04-07 09:57:28 +0900

    起動時の表示の出しわけ

--- lib/ui/screen/login/login_screen.dart
+++ lib/ui/screen/login/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router_builder_sample/provider/app_launch_status.dart';
 import 'package:go_router_builder_sample/provider/auth_notifier.dart';
 
 class LoginScreen extends ConsumerWidget {
@@ -11,14 +12,37 @@ class LoginScreen extends ConsumerWidget {
       appBar: AppBar(
         title: const Text('Login'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            await ref.read(authNotifierProvider.notifier).login();
+      body: switch (ref.watch(appLaunchStatusProvider)) {
+        AsyncLoading() => const Center(child: CircularProgressIndicator()),
+        AsyncError(:final error) => Center(
+            child: Center(
+              child: Text('$error'),
+            ),
+          ),
+        AsyncData(:final value) => switch (value.status) {
+            AppLaunchStatus.loading => const Center(
+                child: CircularProgressIndicator(),
+              ),
+            AppLaunchStatus.error => Center(
+                child: Text(value.exception.toString()),
+              ),
+            AppLaunchStatus.unauthorized => Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('Unauthorized'),
+                    ElevatedButton(
+                      onPressed: () async =>
+                          ref.read(authNotifierProvider.notifier).login(),
+                      child: const Text('Login'),
+                    ),
+                  ],
+                ),
+              ),
+            AppLaunchStatus.authorized => SizedBox.shrink(),
           },
-          child: const Text('Login'),
-        ),
-      ),
+        _ => SizedBox.shrink()
+      },
     );
   }
 }
