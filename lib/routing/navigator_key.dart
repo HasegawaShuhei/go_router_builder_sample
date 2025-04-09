47949362b613c4b19ba00743111898977bc00c7b
HasegawaShuhei <sh1027.dev@gmail.com>
2025-04-09 22:45:42 +0900

    go_routerのファイル削除

--- lib/routing/navigator_key.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-final rootNavigatorKey = GlobalKey<NavigatorState>();
-
-final homeNavigatorKey = GlobalKey<NavigatorState>();
-final taskNavigatorKey = GlobalKey<NavigatorState>();
-final mypageNavigatorKey = GlobalKey<NavigatorState>();
